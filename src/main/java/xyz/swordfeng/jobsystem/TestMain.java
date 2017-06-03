18858c0067516618ffd4222727b6e2fa38121a84
Jianfeng Zhang <swordfeng123@gmail.com>
2017-06-03 09:58:14 -0500

    Update TestMain.java

--- src/main/java/xyz/swordfeng/jobsystem/TestMain.java
+++ src/main/java/xyz/swordfeng/jobsystem/TestMain.java
@@ -5,7 +5,7 @@ import org.rocksdb.RocksDBException;
 public class TestMain {
     public static void main(String[] args) throws User.AuthFailure, User.UserExist, User.MoneyBelowZero {
         DB.getInstance().reset();
-        User u = User.register("swordfeng", "123456", -2147483648);
+        User u = User.register("swordfeng", "123456", Integer.MIN_VALUE);
         User p = User.login("swordfeng", "123456");
         try {
             User r = User.login("swordfeng", "22");
