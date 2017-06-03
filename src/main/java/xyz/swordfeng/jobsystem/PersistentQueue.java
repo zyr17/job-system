4103ccb900e8851625d8af2ccb93d4534621a397
Jianfeng Zhang <swordfeng123@gmail.com>
2017-06-03 16:14:04 +0800

    update persistent queue

--- src/main/java/xyz/swordfeng/jobsystem/PersistentQueue.java
+++ src/main/java/xyz/swordfeng/jobsystem/PersistentQueue.java
@@ -1,14 +1,41 @@
 package xyz.swordfeng.jobsystem;
 
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
 
-abstract public class PersistentQueue<T> extends DB.PersistentData {
-    private Queue<T> queue = new LinkedList();
-    public void push(T v) {
-        queue.add(v);
+public class PersistentQueue<T> {
+    private final String name;
+
+    private PersistentQueue(String name) {
+        this.name = name;
     }
-    public T pop() {
-        return queue.remove();
+
+    public void add(T v) {
+        synchronized (PersistentQueue.class) {
+            DB db = DB.getInstance();
+            LinkedList<T> q = db.get("Persistent/" + name, LinkedList.class);
+            if (q == null) q = new LinkedList<>();
+            q.add(v);
+            db.put("Persistent/" + name, q);
+        }
+    }
+    public T poll() {
+        DB db = DB.getInstance();
+        LinkedList<T> q = db.get("Persistent/" + name, LinkedList.class);
+        if (q == null) q = new LinkedList<>();
+        T result = q.poll();
+        db.put("Persistent/" + name, q);
+        return result;
+    }
+    public List<T> getList() {
+        DB db = DB.getInstance();
+        LinkedList<T> q = db.get("Persistent/" + name, LinkedList.class);
+        if (q == null) q = new LinkedList<>();
+        return q;
+    }
+
+    public static <T> PersistentQueue<T> get(String name) {
+        return new PersistentQueue<T>(name);
     }
 }
