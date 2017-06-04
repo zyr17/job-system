67188df006c4624a15391e11da970feac74b31cd
Jianfeng Zhang <swordfeng123@gmail.com>
2017-06-04 16:53:51 +0800

    use leveldb instead of rocksdb (rocksdb-specific functions are not used)

--- src/main/java/xyz/swordfeng/jobsystem/TestMain.java
+++ src/main/java/xyz/swordfeng/jobsystem/TestMain.java
@@ -1,7 +1,5 @@
 package xyz.swordfeng.jobsystem;
 
-import org.rocksdb.RocksDBException;
-
 public class TestMain {
     public static void main(String[] args) throws User.AuthFailure, User.UserExist, User.MoneyBelowZero {
         DB.getInstance().reset();
